dd08cd2f7d550b7ec2763414cee91321bf92b64b
Luis Armando AA <luis.aguayo@shadesdemexico.com>
2025-01-13 01:05:44 -0600

    creado en dist

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Esto asegura que los archivos construidos se guardan en "dist"
+  },
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'], // Incluye tus archivos
